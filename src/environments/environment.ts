73fe189294a0a090cf16624a7b0cb733c14f0ce2
Natalia <natalia.molla222@gmail.com>
2022-10-17 16:45:38 -0700

    adding things for dark mode feature

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,10 +5,10 @@
 export const environment = {
   production: false,
   withCredentials: true,
-  baseUrl: "https://teamwater.azurewebsites.net",
+  baseUrl: "http://localhost:8080",
   headers: {
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': 'https://teamwater.azurewebsites.net',
+    'Access-Control-Allow-Origin': 'http://localhost:4200/',
   },
 };
 
